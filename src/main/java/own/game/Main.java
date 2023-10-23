d06fd799d58da8d9fd473831cfcf6ab4bd5bc595
kolosov-v <Kolosov-V@gaz-is.ru>
2023-10-24 01:43:19 +0300

    movements

--- src/main/java/own/game/Main.java
+++ src/main/java/own/game/Main.java
@@ -12,6 +12,7 @@ import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 import javafx.scene.canvas.*;
 import own.game.controllers.MovementController;
@@ -20,9 +21,6 @@ import own.game.models.P1Tank;
 
 
 public class Main extends Application {
-
-    private GraphicsContext g;
-    private Image image;
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -36,19 +34,40 @@ public class Main extends Application {
         Pane root = new Pane();
         root.setPrefSize(800, 600);
 
-        image = new Image("./models/tank_p1_40x40.png");
+        P1Tank p1Tank = new P1Tank(800, 600);
+        P1Controller p1Controller = new P1Controller();
 
         Canvas canvas = new Canvas(800, 600);
-        g = canvas.getGraphicsContext2D();
-        g.drawImage(image, 0, 0);
+        canvas.setFocusTraversable(true);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.setFill((Color.rgb(255, 255, 128, 1.0)));
+        gc.fillRect(0, 0, 800, 600);
+
+        canvas.setOnKeyTyped(event -> {
+            if (event.getCharacter().equals("w") || event.getCharacter().equals("ц"))
+                p1Controller.moveUp(p1Tank);
+
+            if (event.getCharacter().equals("s") || event.getCharacter().equals("ы"))
+                p1Controller.moveDown(p1Tank);
+
+            if (event.getCharacter().equals("a") || event.getCharacter().equals("ф"))
+                p1Controller.moveLeft(p1Tank);
+
+            if (event.getCharacter().equals("d") || event.getCharacter().equals("в"))
+                p1Controller.moveRight(p1Tank);
+
+            //non working
+            if (event.getCode() == KeyCode.S)
+                p1Controller.moveDown(p1Tank);
+        });
+
+        //удаление элементов:
+        //root.getChildren().removeAll(bullet.getView(), enemy.getView());
 
         root.getChildren().add(canvas);
+        root.getChildren().add(p1Tank.getImageView());
 
         return root;
-    }
-
-    public void onUpdate() {
-        g.drawImage(image, 0, 0);
     }
 
     public static void main(String[] args) {
--- src/main/java/own/game/controllers/P1Controller.java
+++ src/main/java/own/game/controllers/P1Controller.java
@@ -7,19 +7,27 @@ import own.game.models.P1Tank;
 public class P1Controller {
 
     public void moveUp(P1Tank p1Tank) {
+        p1Tank.getImageView().setY(p1Tank.getY() - 10);
         p1Tank.setY(p1Tank.getY() - 10);
+        p1Tank.getImageView().setRotate(0);
     }
 
     public void moveLeft(P1Tank p1Tank) {
-        p1Tank.setY(p1Tank.getX() - 10);
+        p1Tank.getImageView().setX(p1Tank.getX() - 10);
+        p1Tank.setX(p1Tank.getX() - 10); //todo надо учитывать X середины объекта, а не края
+        p1Tank.getImageView().setRotate(270);
     }
 
     public void moveRight(P1Tank p1Tank) {
-        p1Tank.setY(p1Tank.getX() + 10);
+        p1Tank.getImageView().setX(p1Tank.getX() + 10);
+        p1Tank.setX(p1Tank.getX() + 10);
+        p1Tank.getImageView().setRotate(90);
     }
 
     public void moveDown(P1Tank p1Tank) {
+        p1Tank.getImageView().setY(p1Tank.getY() + 10);
         p1Tank.setY(p1Tank.getY() + 10);
+        p1Tank.getImageView().setRotate(180);
     }
 
 }
