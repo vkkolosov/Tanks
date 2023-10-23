3540b61ad40794414681e92b2c7b3ae3822ebaa8
kolosov-v <Kolosov-V@gaz-is.ru>
2023-10-23 22:23:27 +0300

    еще один лишний коммит

--- src/main/java/own/game/Main.java
+++ src/main/java/own/game/Main.java
@@ -50,4 +50,8 @@ public class Main extends Application {
     public void onUpdate() {
         g.drawImage(image, 0, 0);
     }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
